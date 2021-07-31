5e17e96ed603bb5004cfe02f6cf193ee79fba286
MtheEPIC <mtheepic1423@protonmail.com>
2021-07-31 23:01:14 +0300

    consts file

---
+++ consts.py
@@ -0,0 +1,8 @@
+STOP_MSG = 'Stoping the bot... use /start to start again'
+START_MSG = 'Welcome to Michael\'s meat shop, use /help to get access to the commands'
+HELP_MSG = '/gallery to see the meat\n/order to begin your order'
+GALLERY_MSG = 'use /order to begin your order'
+ORDER_MSG = 'choose a category'
+ORDER_BEEF_MSG = 'beef types:'
+ORDER_CHICKEN_MSG = 'chicken types:'
+UNKNOWN_MSG = 'Sorry, I didn\'t understand that command\nuse /help to see what commands you can use'
