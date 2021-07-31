e0470c6cc491b07274fc7296200636538826bbe6
MtheEPIC <mtheepic1423@protonmail.com>
2021-07-31 22:58:09 +0300

    utils funcs

---
+++ utiles.py
@@ -0,0 +1,42 @@
+import os
+from functools import wraps
+from typing import List, Union
+
+from telegram import InlineKeyboardButton, ChatAction
+
+KEY_LENGTH = 46
+
+
+def send_action(action: ChatAction):
+    """Sends `action` while processing func command."""
+
+    def decorator(func):
+        @wraps(func)
+        def command_func(update, context, *args, **kwargs):
+            context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=action)
+            return func(update, context, *args, **kwargs)
+
+        return command_func
+
+    return decorator
+
+
+def build_menu(
+        buttons: List[InlineKeyboardButton],
+        n_cols: int,
+        header_buttons: Union[InlineKeyboardButton, List[InlineKeyboardButton]] = None,
+        footer_buttons: Union[InlineKeyboardButton, List[InlineKeyboardButton]] = None
+) -> List[List[InlineKeyboardButton]]:
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    if header_buttons:
+        menu.insert(0, header_buttons if isinstance(header_buttons, list) else [header_buttons])
+    if footer_buttons:
+        menu.append(footer_buttons if isinstance(footer_buttons, list) else [footer_buttons])
+    return menu
+
+
+def get_key() -> str:
+    key_file = os.open('key.txt', os.O_RDONLY)
+    key = os.read(key_file, KEY_LENGTH)
+    os.close(key_file)
+    return key.decode()
