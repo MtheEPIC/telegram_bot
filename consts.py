73505e5819717ec9fc826cc6e23918feaf28d7bd
MtheEPIC <mtheepic1423@protonmail.com>
2021-08-01 19:24:54 +0300

    button/keyboard selector

--- consts.py
+++ consts.py
@@ -1,3 +1,5 @@
+import enum
+
 STOP_MSG = 'Stoping the bot... use /start to start again'
 START_MSG = 'Welcome to Michael\'s meat shop, use /help to get access to the commands'
 HELP_MSG = '/gallery to see the meat\n/order to begin your order'
@@ -6,3 +8,11 @@ ORDER_MSG = 'choose a category'
 ORDER_BEEF_MSG = 'beef types:'
 ORDER_CHICKEN_MSG = 'chicken types:'
 UNKNOWN_MSG = 'Sorry, I didn\'t understand that command\nuse /help to see what commands you can use'
+
+
+class Gui(enum.Enum):
+    key = 0
+    button = 1
+
+
+GUI = Gui.key
